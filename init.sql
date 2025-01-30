80f5e526c2a3f6391fc4a100c735f7be0f907589
mirouhh <mirouhh@googlemail.com>
2025-01-30 17:52:53 +0100

    [PostgreSQL] In case of using fixtures SUPERUSER privileges are needed

--- init.sql
+++ init.sql
@@ -1,2 +1,5 @@
 -- Only used for development where Postgres is run in Docker
 create role blogpg with CREATEDB login password 'blogpg';
+
+-- Only used for development where Postgres is run in Docker
+-- create role blogpg with CREATEDB SUPERUSER login password 'blogpg';
